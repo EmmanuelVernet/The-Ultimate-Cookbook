6e60a8da7a4db09aeda4e6e103685ebc9a851ba9
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-26 14:23:51 +0100

    Added recipes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_114229) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_131053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "ingredient_name"
+    t.string "ingredient_category"
+    t.integer "calorie"
+    t.integer "protein"
+    t.integer "glucide"
+    t.integer "lipide"
+    t.integer "fibre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
