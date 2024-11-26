bafc068840668f81310e09d64607b874a4ef823d
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-26 17:50:05 +0100

    Added tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,38 @@
+class TagsController < ApplicationController
+  # only: [:index, :show, :create, :update, :destroy]
+  
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def create
+    @tag = Tag.create(tag_params)
+    @tag.save
+    # TO DO => add safe guard for save
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    @tag.update(tag_params)
+    redirect_to tag_path(@tag)
+    # TO DO +> add safe guard for update withif statement
+  end
+
+  def destroy #allows to remove a tag from a recipe
+    # find the tag item
+    @tag = Tag.find(params[:id])
+    # then destroy
+    @tag.destroy
+    redirect_to tag_path, notice: "Tag deleted"
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+end
