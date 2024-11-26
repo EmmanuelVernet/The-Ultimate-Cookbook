248f4ed6d0952d56aebe1f831a7f286c6c417f46
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-26 16:16:14 +0100

    Added validations and associations on User model for Shares table and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,15 @@ class User < ApplicationRecord
   # FOLLOWEES
   has_many :followees_users, class_name: "FollowersUser", foreign_key: :follower
   has_many :followees, through: :followees_users, source: :follower
+
+  # user shares
+  has_many :user_shares, class_name: "Share", foreign_key: :user
+  has_many :shares, through: :user_shares, source: :user
+  # receiver shares
+  has_many :receiver_shares, class_name: "Share", foreign_key: :receiver
+  has_many :shares, through: :user_shares, source: :receiver
 end
 
 # current_user.followees => renvoie un array de Users que current user follow
 # current_user.followers => renvoie un array de Users qui followent current_user
+# 
