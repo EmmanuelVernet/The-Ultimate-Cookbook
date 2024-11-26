d8c0698b93f02a38b8d5adec9b61f944efa7c61b
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-26 14:24:20 +0100

    Added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_131053) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_132330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,23 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_131053) do
     t.integer "fibre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "recipe_name"
+    t.text "recipe_overview"
+    t.string "recipe_category"
+    t.time "preparation_time"
+    t.string "difficulty"
+    t.string "import_source"
+    t.integer "servings"
+    t.text "recipe_steps"
+    t.integer "recipe_likes"
+    t.boolean "favorite"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -50,4 +67,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_131053) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
