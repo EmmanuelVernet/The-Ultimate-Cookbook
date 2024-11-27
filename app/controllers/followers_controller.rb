e422af461d40c7a9e04374c9779d41b5bbbbd879
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-27 12:49:23 +0100

    Added Ingredient controller updates, Recipes controller, follower controller

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,2 +1,24 @@
 class FollowersController < ApplicationController
+  def index # a user sees all his followers
+    @followers = FollowersUser.all
+  end
+
+  def create # a user follows another
+    @followee = FollowersUser.create(follower_users_params)
+    @followee.save
+  end
+
+  def destroy # unfollow another user 
+    # find the follower
+    @followee = FollowersUser.find(params[:user_id])
+    # then destroy (unfollow)
+    @follower.destroy
+    redirect_to user_followers_path, notice: "Unfollowed"
+  end
+
+  private
+
+  def follower_users_params
+    params.require(:user_id).permit(:follower_id)
+  end
 end
