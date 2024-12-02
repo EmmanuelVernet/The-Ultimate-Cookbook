9b0510a55a19df56f2023abee6dcc731a27f4861
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-12-02 10:00:25 +0100

    Added migration for admin user to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_28_163453) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_02_085513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -142,6 +142,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_28_163453) do
     t.time "available_time"
     t.integer "family_size"
     t.integer "budget"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
