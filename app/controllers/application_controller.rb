d984eed84ff9b9059cc65bdbfd574cede8ce13d2
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2024-12-04 15:40:20 +0100

    first part of checklist done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  # Redirect after sign-in
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  # Set default URL options
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  private
+
+  def set_meta_defaults
+    @meta_title = t('meta_title')
+    @meta_description = t('meta_description')
+    @meta_image = asset_path(t('meta_image'))
   end
 end
